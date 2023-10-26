c18c662be02c533c694c0036165e9c65d52211a2
Royler Marichal Carrazana <52290307+cluzstudio@users.noreply.github.com>
2023-10-26 09:22:55 -0300

    fix server URL

--- src/utils/serverUrl.ts
+++ src/utils/serverUrl.ts
@@ -1,4 +1,4 @@
-const  serverURL = process.env.NODE_ENV === 'development' ?  'http://localhost:8080' : 'https://boilerplateapi.thesaasfactory.dev';
-const  serverURLWS = process.env.NODE_ENV === 'development' ?  'ws://localhost:8080' : 'wss://boilerplateapi.thesaasfactory.dev';
+const  serverURL = process.env.NODE_ENV === 'development' ?  'http://localhost:8080' : 'https://backend-saas-boilerplate.fly.dev';
+const  serverURLWS = process.env.NODE_ENV === 'development' ?  'ws://localhost:8080' : 'wss://backend-saas-boilerplate.fly.dev';
 export  { serverURL,serverURLWS};
 
